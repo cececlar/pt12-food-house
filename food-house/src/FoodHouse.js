d192e71e457c606d7271600211aa7f5b822ec7d3
Cece Claridge <cece.claridge@gmail.com>
2020-03-09 17:59:51 -0400

    dynamic search bar complete

--- food-house/src/FoodHouse.js
+++ food-house/src/FoodHouse.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './App.css';
 import axios from 'axios';
 import SearchForm from './components/SearchForm'
+import DishCard from './components/DishCard'
 
 const FoodHouse = () => {
 
@@ -10,7 +11,7 @@ const FoodHouse = () => {
 
   const fetchData = async () => {
     const result = await axios(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken`
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`
     );
     setData({ hits: result.data.meals });
   };
@@ -23,18 +24,16 @@ const FoodHouse = () => {
  
 
   return (
-    <div>
-      <h1>What's For Dinner?</h1>
-      <SearchForm setSearch={setSearch} />
-      {data.hits.map(meal => {
-        return (
-          <p>{meal.strMeal}</p>
-        )
-      })}
-    </div>
-  )
+    <Fragment>
+      {/* Whoa - are we literally passing a function as a component prop? Yes. Yes we are. */}
+      <SearchForm setSearch={setSearch} initialPlaceholder={search} />
+      <div className="container">
+        {data.hits && data.hits.length
+          ? data.hits.map(meal => <DishCard key={meal.idMeal} meal={meal} />)
+          : "Nothing found :-/"}
+      </div>
+    </Fragment>
+  );
 }
-
-
 
 export default FoodHouse;
